1a44be50fe2176bfbff2657ae55fdf3db0fe7785
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-06-12 08:51:17 -0700

    x

--- Code/PB_JUNE_2023/demo.js
+++ Code/PB_JUNE_2023/demo.js
@@ -1,6 +1,15 @@
 // This is a demo
+/*
 for(var i = 0; i <= 10; i++){
     console.log(i)
 }
 // git
 console.log("cool")
+*/
+var x = 7
+var message = 'boom'
+
+while(x>message.length){
+    console.log(message+x);
+    x--;
+}
