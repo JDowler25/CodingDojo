e97966dc55106add3e7ab47a98bb4e09a1c5ef06
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-06-13 10:27:13 -0700

    new

--- Code/PB_JUNE_2023/morningalgo.js
+++ Code/PB_JUNE_2023/morningalgo.js
@@ -1,9 +1,10 @@
-var countPostives = 0; 
-var numbers = [3,4,-2,7,16,-8,0]
-// each time we loop through the array we want add to countPostives
-for(let i = 0; i<numbers.length; i++){
-    if(i>0){
-        countPostives++
-    }
-}
-console.log("there are", countPostives, "positive values");
+//       06-13-23
+// var countPostives = 0; 
+// var numbers = [3,4,-2,7,16,-8,0]
+// // each time we loop through the array we want add to countPostives
+// for(let i = 0; i<numbers.length; i++){
+//     if(i>0){
+//         countPostives++
+//     }
+// }
+// console.log("there are", countPostives, "positive values");
