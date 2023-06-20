bf9b70e51dcd9dec04ba3810e7126c0e428664ee
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-06-20 13:28:55 -0700

    new

--- Code/PB_JUNE_2023/morningalgo.js
+++ Code/PB_JUNE_2023/morningalgo.js
@@ -226,55 +226,144 @@ JS has the .at() method for this purpose, but solve this algo w/o it.
 // .push() that element into the new array 
 // After the loop is done we will return the arr.
 
-//             0    1    2    3    4
-const arr1 = ["a", "b", "c", "d", "e"];
-const startIdx1 = 0;
-const endIdx1 = 5;
-const expected1 = ["a", "b", "c", "d", "e"];
+//           0    1    2    3    4
+// const arr1 = ["a", "b", "c", "d", "e"];
+// const startIdx1 = 0;
+// const endIdx1 = 5;
+// const expected1 = ["a", "b", "c", "d", "e"];
 
-const arr2 = ["a", "b", "c", "d", "e"];
-const startIdx2 = 0;
-const endIdx2 = 1;
-const expected2 = ["a"];
+// const arr2 = ["a", "b", "c", "d", "e"];
+// const startIdx2 = 0;
+// const endIdx2 = 1;
+// const expected2 = ["a"];
 
-//            0   1    2   3   4
-const arr3 = [12, 24, 36, 48, 60];
-const startIdx3 = 1;
-const endIdx3 = 3;
-const expected3 = [24, 36];
+// //            0   1    2   3   4
+// const arr3 = [12, 24, 36, 48, 60];
+// const startIdx3 = 1;
+// const endIdx3 = 3;
+// const expected3 = [24, 36];
 
-const arr4 = ["a", "b", "c", "d", "e"];
-const startIdx4 = -100;
-const endIdx4 = 100;
-const expected4 = ["a", "b", "c", "d", "e"];
+// const arr4 = ["a", "b", "c", "d", "e"];
+// const startIdx4 = -100;
+// const endIdx4 = 100;
+// const expected4 = ["a", "b", "c", "d", "e"];
 
-const arr5 = ["a", "b", "c", "d", "e"];
-const startIdx5 = 0;
-const endIdx5 = 0;
-const expected5 = [];
+// const arr5 = ["a", "b", "c", "d", "e"];
+// const startIdx5 = 0;
+// const endIdx5 = 0;
+// const expected5 = [];
 
-const arr6 = ["a", "b", "c", "d", "e"];
-const startIdx6 = 1;
-const endIdx6 = 1;
-const expected6 = [];
+// const arr6 = ["a", "b", "c", "d", "e"];
+// const startIdx6 = 1;
+// const endIdx6 = 1;
+// const expected6 = [];
 
-//* choose your ALGO ROLES
-// DRIVER 🚗
-// PRESENTER 👨‍🏫
-// NAVIGATOR 🧭
+// //* choose your ALGO ROLES
+// // DRIVER 🚗
+// // PRESENTER 👨‍🏫
+// // NAVIGATOR 🧭
 
-/*
-    👉 spend 10 mins writing the pseudocode only!
+// /*
+//     👉 spend 10 mins writing the pseudocode only!
 
-*/
-// RIOT
+// */
+// // RIOT
 
-function slice(items, startIdx, endIdx){
-    let newArray = [];
-    for(let i = startIdx; i< endIdx; i++){
-        if(i >= 0 && i < items.length){
-         newArray.push(items[i])
-        }
-    }
-    return newArray;
-}
+// function slice(items, startIdx, endIdx){
+//     let newArray = [];
+//     for(let i = startIdx; i< endIdx; i++){
+//         if(i >= 0 && i < items.length){
+//          newArray.push(items[i])
+//         }
+//     }
+//     return newArray;
+// }
+
+//          06-20-23
+// So you want to be a JS objects master?
+// var monster = {
+//     id: 1,
+//     name: "Bulbasaur",
+//     types: ["poison", "grass"]
+// };
+
+// console.log(monster.name + " " + monster.types[1])
+
+
+var pokemon = [
+    { "id": 1, "name": "Bulbasaur", "types": ["poison", "grass"] },
+    { "id": 5, "name": "Charmeleon", "types": ["fire"] },
+    { "id": 9, "name": "Blastoise", "types": ["water"] },
+    { "id": 12, "name": "Butterfree", "types": ["bug", "flying"] },
+    { "id": 16, "name": "Pidgey", "types": ["normal", "flying"] },
+    { "id": 23, "name": "Ekans", "types": ["poison"] },
+    { "id": 24, "name": "Arbok", "types": ["poison"] },
+    { "id": 25, "name": "Pikachu", "types": ["electric"] },
+    { "id": 37, "name": "Vulpix", "types": ["fire"] },
+    { "id": 52, "name": "Meowth", "types": ["normal"] },
+    { "id": 63, "name": "Abra", "types": ["psychic"] },
+    { "id": 67, "name": "Machamp", "types": ["fighting"] },
+    { "id": 72, "name": "Tentacool", "types": ["water", "poison"] },
+    { "id": 74, "name": "Geodude", "types": ["rock", "ground"] },
+    { "id": 87, "name": "Dewgong", "types": ["water", "ice"] },
+    { "id": 98, "name": "Krabby", "types": ["water"] },
+    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+    { "id": 122, "name": "Mr. Mime", "types": ["psychic"] },
+    { "id": 133, "name": "Eevee", "types": ["normal"] },
+    { "id": 144, "name": "Articuno", "types": ["ice", "flying"] },
+    { "id": 145, "name": "Zapdos", "types": ["electric", "flying"] },
+    { "id": 146, "name": "Moltres", "types": ["fire", "flying"] },
+    { "id": 148, "name": "Dragonair", "types": ["dragon"] }
+];
+// give the first pokemon name
+
+// log the name of the first pokemon
+// console.log(pokemon[0].types[0])
+
+// // If we wanted to console.log the names of the pokémon who have an id greater than 99 we could...
+// console.log("The names of the pokemon who have an id greater than 99:")
+
+// for(var i = 0; i < pokemon.length; i++) {
+//     if (pokemon[i].id > 99) {
+//         console.log(pokemon[i].name)
+//     }
+//     // console.log(pokemon[i].id)
+// }
+
+// Challenges - using the array of pokémon above and loops:
+// 1. console.log the pokémon objects whose id is evenly divisible by 3
+// for(var i = 0; i < pokemon.length; i++) {
+//     if (pokemon[i].id % 3 == 0) {
+//         console.log(pokemon[i])
+//     }
+// } 
+    // 2. console.log the pokémon objects that have more than one type
+// for(var i = 0; i < pokemon.length; i++) {
+//         if (pokemon[i].types[1]) {
+//             console.log(pokemon[i])
+//         } 
+//     }
+    // 3. console.log the names of the pokémon whose only type is "poison",
+// for(var i = 0; i < pokemon.length; i++) {
+//             if (pokemon[i].types[0] == "poison" && pokemon[i].types.length == 1) {
+//                 console.log(pokemon[i].name)
+//             } 
+//         }
+
+        
+// 4. console.log the first type of all the pokémon whose second type is "flying"
+// for(var i = 0; i < pokemon.length; i++) {
+//             if (pokemon[i].types[1] == "flying") {
+//                 console.log(pokemon[i].types[0])
+//             } 
+//         }
+        
+        
+        
+// BONUS Challenge: console.log the reverse of the names of the pokémon whose only type is poison.   
+// for(var i = pokemon.length; pokemon.length >= 0; i--) {
+//             if (pokemon[i].types[0] == "poison" && pokemon[i].types.length == 1) {
+//                 console.log(pokemon[i].name)
+//             } 
+//         }
+                
---
+++ Code/Web_Fundamentals/JavaScript/demo/demo.js
@@ -0,0 +1,10 @@
+function over(element) {
+    element.style.backgroundColor = "lime";    
+}
+    
+function out(element) {
+    element.style.backgroundColor = "silver";   
+}
+
+
+
