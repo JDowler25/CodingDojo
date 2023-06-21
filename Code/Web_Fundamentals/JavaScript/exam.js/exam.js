4f5a0388b7d90b6bf31c50688188276cf5eed518
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-06-21 13:10:32 -0700

    new

---
+++ Code/Web_Fundamentals/JavaScript/exam.js/exam.js
@@ -0,0 +1,13 @@
+function remove(id) {
+    let alert = document.getElementById('alert');
+    alert.remove();
+}
+
+function popUp() {
+    alert("Shopping Cart is empty!");
+}
+
+function changeImage(img){
+    let change = document.getElementById('img');
+    change.src = "./images/assets/succulents-2.jpg";
+}
