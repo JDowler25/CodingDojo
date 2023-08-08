b5ea7108b0c8c36deb4aee82ab4bb3ab51eaa51b
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-08-07 21:45:06 -0600

    new work

---
+++ Code/JavaScript/JS_WeekOne/REACT/new-react-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <p>
+        *Learn React
+      </p>
+      <p>
+        *Climb Mt. Everest
+      </p>
+      <p>
+        *Run a marathon
+      </p>
+      <p>
+        *Feed the dogs
+      </p>
+    </div>
+  );
+}
+
+export default App;
