228401cda255870ae1e2cf5a0ae0b341f8e984aa
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-08-16 09:36:34 -0600

    jokes API

--- Code/JavaScript/MongoDB/Server/config/mongoose.config.js
+++ Code/JavaScript/MongoDB/Server/config/mongoose.config.js
@@ -1,7 +1,9 @@
 // 1. import mongoose
 const mongoose = require('mongoose');
 // 2. create a uri variable that shows the connection url. 
+
 // 2.1 Make sure you use .env for the sensitive information like the link or password or username
+
 // 2.2 Make sure you console log and check if the route is good before you connect to the database. 
 
 const uri = process.env.URI
@@ -11,5 +13,6 @@ mongoose.connect(uri)
     .then(() => console.log("Established a connection to the database"))
     .catch(err => console.log("Something went wrong when connecting to the database", err));
 // Check the code from the platform
+
 // You may need to go to Atlas to update the IP access
 
--- Code/JavaScript/MongoDB/Server/controllers/destination.controller.js
+++ Code/JavaScript/MongoDB/Server/controllers/destination.controller.js
@@ -1,5 +1,6 @@
 // 1. import the model
 const Destination = require('../models/destination.model')
+
 // 2. export all the functions with placeholder
 module.exports.apiTest = (req, res)=>{
     res.json({message: "ok"})
@@ -9,15 +10,19 @@ module.exports.apiTest = (req, res)=>{
 module.exports.allDest = (req, res)=>{
     // CODE TO BE COMPLETED
 }
+
 module.exports.oneDest = (req, res)=>{
     // CODE TO BE COMPLETED
 }
+
 module.exports.addDest = (req, res)=>{
     // CODE TO BE COMPLETED
 }
+
 module.exports.updateDest = (req, res)=>{
     // CODE TO BE COMPLETED
 }
+
 module.exports.deleteDest = (req, res)=>{
     // CODE TO BE COMPLETED
 }
