0ab64b82c2800bfe3353e8cc5a65f6f332396444
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-08-22 09:13:14 -0600

    Validation bug fix

--- Code/JavaScript/Full-Stack-MERN/Authors/client/src/components/CreatePage.jsx
+++ Code/JavaScript/Full-Stack-MERN/Authors/client/src/components/CreatePage.jsx
@@ -4,24 +4,47 @@ import { useNavigate } from 'react-router-dom';
 
 const CreatePage = () => {
   const [name, setName] = useState("")
-
+  const [errors, setErrors] = useState([]);
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    axios.post('http://localhost:8000/api/authors', { name: name})
-      .then(response => {
-        navigate('/authors')
+    e.preventDefault();
+  
+    
+    // setErrors({});
+  
+    axios.post('http://localhost:8000/api/authors', { name: name })
+      .then(res => {
+        console.log('then')
+        setName(name);
+        console.log(name);
+        navigate('/authors');
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        // if (err.response && err.response.data) {
+        //   console.log("setErrors")
+        //   setErrors(err.response.data.errors); 
+        // } else {
+        //   console.log("err");
+        // }
+        console.log("setErrors")
+        const errorResponse = err.response.data.errors;
+        const errorArr = []
+        for(const key of Object.keys(errorResponse)){
+          errorArr.push(errorResponse[key].message)
+        }
+        setErrors(errorArr); 
+      });
   }
   return (
     <div>
       <h1>Add a new Author</h1>
       <form onSubmit={handleSubmit} className='form'>
+        {errors.map((err,idx) => <p key={idx}>{err}</p>)}
         <div className='mb-3'>
           <label className='form-label'>Name</label>
           <input type='text' name='name' value={name} onChange={e => setName(e.target.value)} className='form-control' placeholder="add Name here..." />
+          {/* {errors.name && <p className="text-danger">{errors.name.message}</p>} */}
         </div>
         <button type='submit' className='btn btn-primary'>Submit</button>
       </form>
--- Code/JavaScript/Full-Stack-MERN/Authors/server/controllers/Authors.controller.js
+++ Code/JavaScript/Full-Stack-MERN/Authors/server/controllers/Authors.controller.js
@@ -9,25 +9,23 @@ module.exports.apiTest = (req, res) => {
 module.exports.allAuthors = (req, res) => {
     Authors.find()// returns lists of objects
         .then(authorList => res.json(authorList))
-        .catch((err) => {
-            res.json(err)
-        });
+        .catch(err => res.status(400).json(err))
 }
 
 module.exports.oneAuthor = (req, res) => {
     Authors.findOne({ _id: req.params.id })
         .then(oneAuthor => res.json(oneAuthor))
-        .catch((err) => {
-            res.json(err)
-        });
+        .catch(err => res.status(400).json(err))
 }
 
 module.exports.addAuthor = (req, res) => {
     Authors.create(req.body)
     .then(newAuthor => res.json(newAuthor))
-    .catch((err) => {
-        res.json(err)
-    });
+    .catch(err =>{
+        console.log(err)
+        res.status(400).json(err)
+    }
+    )
 }
 
 module.exports.updateAuthor = (req, res) => {
@@ -35,19 +33,15 @@ module.exports.updateAuthor = (req, res) => {
         {_id: req.params.id},//criteria,
         req.body,//info to be updated
         {new:true, runValidators:true}
-        //new: specify the return response
+        //new: specify the return res
         //runValidators: enable validation in update
         )
         .then(updatedAuthor => res.json(updatedAuthor))
-        .catch((err) => {
-            res.json(err)
-        });
+        .catch(err => res.status(400).json(err))
 }
 
 module.exports.deleteAuthor = (req, res) => {
     Authors.deleteOne({ _id: req.params.id })
         .then(status => res.json(status))
-        .catch((err) => {
-            res.json(err)
-        });
+        .catch(err => res.status(400).json(err))
 }
