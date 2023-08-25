5bb50fd2badb88ccb76650e272b0981c4f7d7e8b
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-08-25 09:09:35 -0600

    3D react JS portfolio

--- Code/JavaScript/MERN_Project/my-3d-portfolio/src/assets/index.js
+++ Code/JavaScript/MERN_Project/my-3d-portfolio/src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./jd-logo-white.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -20,7 +20,10 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import python from "./tech/python.png";
+import java from "./tech/java.png";
 
+import codingDojo from "./company/codingDojo.png";
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
@@ -59,4 +62,7 @@ export {
   carrent,
   jobit,
   tripguide,
+  codingDojo,
+  python,
+  java,
 };
---
+++ Code/JavaScript/MERN_Project/readme.md
@@ -0,0 +1,4 @@
+# First steps to setting up folder structure
+1. mkdir (replace with foldername)
+2. cd (your folder)
+3. npm init vite (projectname)
