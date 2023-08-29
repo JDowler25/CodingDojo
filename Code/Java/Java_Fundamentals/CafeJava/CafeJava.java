7bdbf8a50213dc7511b2f9a89134ea5f84e30f93
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-08-29 12:28:05 -0600

    Cafe Java

--- Code/Java/Java_Fundamentals/CafeJava/CafeJava.java
+++ Code/Java/Java_Fundamentals/CafeJava/CafeJava.java
@@ -1,5 +1,3 @@
-package CafeJava;
-
 public class CafeJava {
     public static void main(String[] args) {
         // APP VARIABLES
@@ -11,16 +9,53 @@ public class CafeJava {
         
         // Menu variables (add yours below)
         double mochaPrice = 3.5;
+        double dripCoffee = 6.69;
+        double dripLatte = 6.99;
+        double dripCappuccino = 7.50;
     
         // Customer name variables (add yours below)
         String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+
     
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
+
     
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
         // Example:
         System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        if (isReadyOrder1){
+            System.out.println(customer1 + readyMessage);
+        } else {
+            System.out.println(customer1 + pendingMessage);
+        }
     	// ** Your customer interaction print statements will go here ** //
+        System.out.println(generalGreeting + customer4); 
+        if (isReadyOrder2) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + dripCappuccino);
+        } else{
+            System.out.println(customer4 + pendingMessage);
+        }
+
+        System.out.println(generalGreeting + customer2);
+        int latteOrder = 2;
+        double totalSam = dripLatte * latteOrder;
+        if (isReadyOrder3) {
+            System.out.println(customer2 + readyMessage);
+            System.out.println(displayTotalMessage + totalSam);
+        } else {
+            System.out.println(customer2 + pendingMessage);
+        } 
+
+        System.out.println(generalGreeting + customer3);
+        double correctTotal = dripLatte - dripCoffee;
+        System.out.println(displayTotalMessage + correctTotal);
     }
 }
