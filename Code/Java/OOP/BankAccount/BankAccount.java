170479e7b86975b234280217d0846d370db7bf3e
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-09-01 11:45:45 -0600

    Bank Account assignment

--- Code/Java/OOP/BankAccount/BankAccount.java
+++ Code/Java/OOP/BankAccount/BankAccount.java
@@ -1,9 +1,7 @@
-
 public class BankAccount {
     // MEMBER VARIABLES
     private double checkingBalance;
     private double savingsBalance;
-    private String accountNumber;
 
     private static int accounts;
     private static double totalMoney; // refers to the sum of all bank account checking and savings balances
@@ -11,9 +9,9 @@ public class BankAccount {
     // CONSTRUCTOR
     // Be sure to increment the number of accounts
     public BankAccount() {
+        BankAccount.accounts++;
         this.checkingBalance = 0.0;
         this.savingsBalance = 0.0;
-        accounts++;
     }
 
     // GETTERS
@@ -22,20 +20,24 @@ public class BankAccount {
         return this.checkingBalance;
     }
 
+    public void setCheckingBalance(double checkingBalance) {
+        this.checkingBalance = checkingBalance;
+    }
+
     public double getSavingsBalance() {
         return this.savingsBalance;
     }
 
-    public static int getNumberOfAccounts() {
-        return accounts;
+    public void setSavingsBalance(double savingsBalance) {
+        this.savingsBalance = savingsBalance;
+    }
+
+    public static int getAccounts() {
+        return BankAccount.accounts;
     }
 
     public static double getTotalMoney() {
-        return totalMoney;
-    }
-
-    public String getAccountNumber() {
-        return this.accountNumber;
+        return BankAccount.totalMoney;
     }
 
     // METHODS
@@ -43,38 +45,34 @@ public class BankAccount {
     // - users should be able to deposit money into their checking or savings
     // account
     public void deposit(String accountType, double amount) {
-        if (amount > 0) {
-            if (accountType.equals("checking")) {
-                checkingBalance += amount;
-            } else if (accountType.equals("ssaving")) {
-                savingsBalance += amount;
-            }
-            totalMoney += amount;
-            System.out.println("Deposit $" + amount + " into " + accountType + " account.");
-        } else {
-            System.out.println("Invalid deposit amount.");
+        if( accountType == "checking") {
+            this.checkingBalance += amount;
+        } else if ( accountType == "savings") {
+            this.savingsBalance += amount; 
         }
+        BankAccount.totalMoney += amount;
     }
 
-    // withdraw
+
+     // withdraw
     // - users should be able to withdraw money from their checking or savings
     // account
     // - do not allow them to withdraw money if there are insufficient funds
     // - all deposits and withdrawals should affect totalMoney
     public void withdraw(String accountType, double amount) {
         if (amount > 0) {
-            if (accountType.equals("checking")) {
+            if (accountType == "checking") {
                 if (checkingBalance >= amount) {
                     checkingBalance -= amount;
-                    totalMoney -= amount;
+                    BankAccount.totalMoney -= amount;
                     System.out.println("Withdrawn $" + amount + " from " + accountType + " account.");
                 } else {
                     System.out.println("Insufficient funds in checking account.");
                 }
-            } else if (accountType.equals("savings")) {
+            } else if (accountType == "savings") {
                 if (savingsBalance >= amount) {
                     savingsBalance -= amount;
-                    totalMoney -= amount;
+                    BankAccount.totalMoney -= amount;
                     System.out.println("Withdrawn $" + amount + " from " + accountType + " account.");
                 } else {
                     System.out.println("Insufficient funds in savings account.");
@@ -92,4 +90,4 @@ public class BankAccount {
         System.out.println("Checking Account Balance: $" + checkingBalance);
         System.out.println("Savings Account Balance: $" + savingsBalance);
     }
-}
+} 
--- Code/Java/OOP/BankAccount/BankTest.java
+++ Code/Java/OOP/BankAccount/BankTest.java
@@ -5,22 +5,29 @@ public class BankTest {
         BankAccount account2 = new BankAccount();
         BankAccount account3 = new BankAccount();
 
+        System.out.println(BankAccount.getAccounts());
+
         // Deposit Test
         // - deposit some money into each bank account's checking or savings account and display the balance each time
         // - each deposit should increase the amount of totalMoney
         account1.deposit("checking", 1000.0);
+        System.out.println(account1.getCheckingBalance()); 
         account2.deposit("savings", 500.0);
+        System.out.println(account2.getSavingsBalance());
         account3.deposit("checking", 200.0);
-
+        System.out.println(account3.getCheckingBalance());
         // Withdrawal Test
         // - withdraw some money from each bank account's checking or savings account and display the remaining balance
         // - each withdrawal should decrease the amount of totalMoney
         account1.withdraw("checking", 200.0);
+        System.out.println(account1.getCheckingBalance()); 
         account2.withdraw("savings", 500.0);
+        System.out.println(account2.getSavingsBalance());
         account3.withdraw("checking", 300.0);
-        
+        System.out.println(account3.getCheckingBalance());
+
         // Static Test (print the number of bank accounts and the totalMoney)
-        System.out.println("Number of Bank Accounts: " + BankAccount.getNumberOfAccounts());
+        System.out.println("Number of Bank Accounts: " + BankAccount.getAccounts());
         System.out.println("Total Money in Bank: $" + BankAccount.getTotalMoney());
     }
 }
