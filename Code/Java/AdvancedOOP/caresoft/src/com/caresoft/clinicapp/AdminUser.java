bef6d0e9b48a98ec76c78f3faadabd6082226831
Jaydan Dowler <Jaydan.Dowler25@gmail.com>
2023-09-05 19:14:46 -0600

    Caresoft assignment

--- Code/Java/AdvancedOOP/caresoft/src/com/caresoft/clinicapp/AdminUser.java
+++ Code/Java/AdvancedOOP/caresoft/src/com/caresoft/clinicapp/AdminUser.java
@@ -6,16 +6,14 @@ import java.util.Date;
 public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {
 	private Integer employeeID;
     private String role;
-    private ArrayList<String> securityIncidents;
+    private ArrayList<String> securityIncidents = new ArrayList<>();
     
     // TO DO: Implement a constructor that takes an ID and a role
     // TO DO: Implement HIPAACompliantUser!
     // TO DO: Implement HIPAACompliantAdmin!
     
     public AdminUser(int i, String string) {
-        this.id = id;
-        this.role = role;
-        this.securityIncidents = new ArrayList<>();
+        this.id = i; 
     }
     public void newIncident(String notes) {
         String report = String.format(
@@ -31,28 +29,29 @@ public class AdminUser extends User implements HIPAACompliantUser, HIPAAComplian
         );
         securityIncidents.add(report);
     }
+    
     @Override
     public ArrayList<String> reportSecurityIncidents() {
         return this.securityIncidents;
     }
+
     @Override
     public boolean assignPin(int pin) {
         String thePin = Integer.toString(pin);
-        if(thePin.length() >= 6) {
+        if(thePin.length() >= 6){
             this.pin = pin;
-            return true; 
-        } else {
-            return false;
+            return true;
         }
+        return false;
     }
     @Override
     public boolean accessAuthorized(Integer confirmedAuthID) {
-        if(!this.id.equals(confirmedAuthID)) {
-            authIncident();
-            return false;
+        if(confirmedAuthID == this.id){
+            return true;
         }
-        return true;
-    }
+        authIncident();
+        return false;
+    } 
     
     // TO DO: Setters & Getters
 }
--- Code/Java/AdvancedOOP/caresoft/src/com/caresoft/clinicapp/Physician.java
+++ Code/Java/AdvancedOOP/caresoft/src/com/caresoft/clinicapp/Physician.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Physician extends User implements HIPAACompliantUser {
-    private ArrayList<String> patientNotes;
-
-    // TO DO: Constructor that takes an IDcopy
+	private ArrayList<String> patientNotes;
+	
+	// TO DO: Constructor that takes an IDcopy
     // TO DO: Implement HIPAACompliantUser!
-
-    public Physician(int id) {
-        this.id = id;
-        this.patientNotes = new ArrayList<>();
+	
+	public Physician(int i) {
+        this.id = i;
     }
 
     public void newPatientNotes(String notes, String patientName, Date date) {
         String report = String.format(
-                "Datetime Submitted: %s \n", date);
+            "Datetime Submitted: %s \n", date);
         report += String.format("Reported By ID: %s\n", this.id);
         report += String.format("Patient Name: %s\n", patientName);
         report += String.format("Notes: %s \n", notes);
@@ -26,18 +25,20 @@ public class Physician extends User implements HIPAACompliantUser {
     @Override
     public boolean assignPin(int pin) {
         String thePin = Integer.toString(pin);
-        if (thePin.length() == 4) {
+        if(thePin.length() == 4){
             this.pin = pin;
             return true;
-        } else{
-            return false;
         }
+        return false;
     }
 
     @Override
     public boolean accessAuthorized(Integer confirmedAuthID) {
-        return this.id.equals(confirmedAuthID);
+        if(confirmedAuthID == this.id){
+            return true;
+        }
+        return false;
     }
-
+	
     // TO DO: Setters & Getters
 }
